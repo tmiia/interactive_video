13d12647629ea5f028c30eeb21526d16a6340db4
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-11-07 01:02:30 +0100

    cleaning: removing some console.log

--- src/assets/js/sequences/motion.js
+++ src/assets/js/sequences/motion.js
@@ -11,14 +11,10 @@ export default class Motion extends Sequence {
     super.play()
     this.context.querySelector('video').play()
     this.animate()
-    console.log("MOTIOOOOON", this.motion);
   }
 
   animate() {
-    console.log("MOTIOOOOON ANIMATE", this.motion);
     const tl = gsap.timeline();
-    console.log(this.motion);
-
     tl.to(this.motion, {zIndex: 1000, duration: 0.3, delay: 0.5});
     tl.to(this.motion, {zIndex: 5, duration: 0.5, delay: 8.5});
   }
