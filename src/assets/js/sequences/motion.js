2d0edd3c30c150fe8ae6286e82d4e897cfc4df16
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-11-07 01:40:26 +0100

    feat: toss feature added to motion / paper ball sequence

--- src/assets/js/sequences/motion.js
+++ src/assets/js/sequences/motion.js
@@ -14,9 +14,42 @@ export default class Motion extends Sequence {
   }
 
   animate() {
-    const tl = gsap.timeline();
+    const tl = gsap.timeline({
+      onComplete: () => {
+        this.tossIt();
+      }
+    });
+
     tl.to(this.motion, {zIndex: 1000, duration: 0.3, delay: 0.5});
     tl.to(this.motion, {zIndex: 5, duration: 0.5, delay: 8.5});
+    tl.to(this.motion, {opacity: 0, duration: 0});
+  }
+
+  tossIt() {
+    const ball = document.querySelector(".js-ball");
+
+    ball.addEventListener("mousedown", () => {
+      const onMouseMove = (event) => {
+
+        ball.style.left = `${event.clientX}px`;
+        ball.style.top = `${event.clientY}px`;
+      };
+
+      document.addEventListener("mousemove", onMouseMove);
+
+      document.addEventListener("mouseup", () => {
+        if (ball && (
+            ball.getBoundingClientRect().top < 0 ||
+            ball.getBoundingClientRect().left < 0 ||
+            ball.getBoundingClientRect().bottom > window.innerHeight ||
+            ball.getBoundingClientRect().right > window.innerWidth)) {
+
+          this.onComplete();
+        }
+        document.removeEventListener("mousemove", onMouseMove);
+      }, { once: true });
+    });
+
   }
 
   stop(){
