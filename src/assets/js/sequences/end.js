e8189cf781e3554e34b44db0265463fbaf7a3517
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-11-08 14:21:27 +0100

    feat: end video added

--- src/assets/js/sequences/end.js
+++ src/assets/js/sequences/end.js
@@ -3,10 +3,12 @@ import Sequence from "../sequence.js";
 export default class Title extends Sequence {
   constructor(context, rank) {
     super(context, rank);
+    this.motion = this.context.querySelector('.motion');
   }
 
   play(){
     super.play()
+    this.context.querySelector('video').play()
   }
 
   stop(){
